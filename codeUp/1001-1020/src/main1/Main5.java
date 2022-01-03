bb40b4f5e9320ecd6a856a724a6fead1d295bfae
weather <goodhik8@daum.net>
2022-01-03 18:06:17 +0900

    [220103] 기초100제 1001-1010번 문제 풀이

---
+++ codeUp/1001-1020/src/main1/Main5.java
@@ -0,0 +1,8 @@
+package main1;
+
+public class Main5 {
+	public static void main(String[] args) {
+		// 220103 풀이 1005번 문제
+		System.out.println("\"Hello World\"");
+	}
+}
---
+++ codeUp/1001-1020/src/main1/Main6.java
@@ -0,0 +1,8 @@
+package main1;
+
+public class Main6 {
+	public static void main(String[] args) {
+		// 220103 풀이 1006번 문제
+		System.out.println("\"!@#$%^&*()\"");
+	}
+}
