40312997f28c2b27e077d81c350c11c56ede8472
weather <goodhik8@daum.net>
2022-01-06 16:41:46 +0900

    Delete CodeUp_Java/Main1-11 directory

--- CodeUp_Java/Main1-11/Main6.java
+++
@@ -1,8 +0,0 @@
-package main1;
-
-public class Main6 {
-	public static void main(String[] args) {
-		// 220103 풀이 1006번 문제
-		System.out.println("\"!@#$%^&*()\"");
-	}
-}
--- CodeUp_Java/Main1-11/Main7.java
+++
@@ -1,8 +0,0 @@
-package main1;
-
-public class Main7 {
-	public static void main(String[] args) {
-		// 220103 풀이 1007번 문제
-		System.out.println("\"C:\\Download\\hello.cpp\"");
-	}
-}
