93934fbd36e467d821b12b3b1b56606f3da4fbb3
weather <goodhik8@daum.net>
2022-01-06 16:42:59 +0900

    [220103] 기초100제 1001-1011번 문제 풀이

---
+++ CodeUp/1001-1020/src/main1/Main3.java
@@ -0,0 +1,8 @@
+package main1;
+
+public class Main3 {
+	public static void main (String[] args) {
+		// 220103 풀이 1003번 문제
+		System.out.println("Hello\nWorld");
+	}
+}
---
+++ CodeUp/1001-1020/src/main1/Main7.java
@@ -0,0 +1,8 @@
+package main1;
+
+public class Main7 {
+	public static void main(String[] args) {
+		// 220103 풀이 1007번 문제
+		System.out.println("\"C:\\Download\\hello.cpp\"");
+	}
+}
